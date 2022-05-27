564a040ba8105f0286ada043b3556cfeae476c46
GadStam <gadstam71@gmail.com>
2022-05-27 17:25:26 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, ImageBackground} from 'react-native';
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={require('./assets/water.jpg')} resizeMode="cover" style={styles.backgroundImage}>
+      <Text>hello !</Text>
+      <Image style={styles.logo} source={require('./assets/logoMI2.png')}></Image>
+        </ImageBackground>
+    
     </View>
   );
 }
@@ -17,4 +21,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 350,
+    height: 200,
+},
+    
+    backgroundImage: {
+      flex: 1,
+      width: '100%',
+      height: '100%',
+      resizeMode: 'cover',        
+  }
+   
 });
