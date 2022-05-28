4130d97e367ecde79c0a85cc002e982b7e831282
GadStam <gadstam71@gmail.com>
2022-05-28 00:36:03 -0300

    mejorar botones

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, ImageBackground} from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, Button, TouchableOpacity} from 'react-native';
+
 
 export default function App() {
   return (
     
     <View style={styles.container}>
-      <ImageBackground source={require('./assets/water.jpg')} resizeMode="cover" style={styles.backgroundImage}>
-      <Text>hello !</Text>
+      <ImageBackground source={require('./assets/blue.jpg')} resizeMode="cover" style={styles.backgroundImage}>
       <Image style={styles.logo} source={require('./assets/logoMI2.png')}></Image>
+      <Button style={styles.Buttoner} title="Outline"></Button>
+
+      <Button color="orange" title="Outline"></Button>
         </ImageBackground>
     
     </View>
   );
 }
+
+
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -26,6 +37,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: 350,
     height: 200,
+    marginTop: 40,
 },
     
     backgroundImage: {
@@ -33,6 +45,13 @@ const styles = StyleSheet.create({
       width: '100%',
       height: '100%',
       resizeMode: 'cover',        
+  },
+  Buttoner:{
+  marginTop:50
+
   }
+  
+
+  
    
 });
